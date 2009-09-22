f52f1fe4a38ef90d7bc7807e5805ab67367fef03
Ryan Bigg <radarlistener@gmail.com>
2009-09-22 22:21:32 +1000

    Don't create build directory

--- app/models/build_job.rb
+++ app/models/build_job.rb
@@ -10,7 +10,6 @@ class BuildJob < Struct.new(:build_id, :payload)
     end
     
     build_directory = "/data/builds/#{repository["name"]}"
-    FileUtils.mkdir_p(build_directory)
     build.update_attribute("status", "setting up repository")
     # Will have to have different directories for the different branches at one point.
     if !File.exist?(File.join(build_directory, ".git"))
