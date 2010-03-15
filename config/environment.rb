b8b1301b5de27e69bf7563dd1704c93e632d7ed2
Michael Bumann <michael@railslove.com>
2010-03-15 16:21:16 +0100

    remove god from env dependencies

--- config/environment.rb
+++ config/environment.rb
@@ -10,7 +10,6 @@ Rails::Initializer.run do |config|
   config.gem 'json'
   config.gem 'popen4'
   config.gem 'nokogiri', :version => ">= 1.3.2"
-  config.gem 'god'
   config.gem 'SystemTimer', :lib => "system_timer" # see http://ph7spot.com/articles/system_timer
   config.gem 'grit'
   config.time_zone = 'UTC'
